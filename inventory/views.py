8f7f8f647f38a4068032344ba0a52634eb457242
afiori <afiori@localhost.localdomain>
2016-01-06 14:10:07 +0000

    Adding CSS and JS with staticfiles settings, upgrading templates

--- inventory/views.py
+++ inventory/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render
 from django.http import Http404
-# from django.http import HttpResponse
+
+from inventory.models import Item
 
 def index(request):
     items = Item.objects.exclude(amount=0)
     return render(request, 'inventory/index.html', {
          'items': items,
     })
-    # return HttpResponse('<p>In index view</p>')
 
 def item_detail(request, id):
     try:
         item = Item.objects.get(id=id)
-    except Item.DoeNotExist:
+    except Item.DoesNotExist:
         raise Http404('This item does not exist')
-    return render(request, 'inventory/item_detail.html')
-    # return HttpResponse('<p>In item_detail view with id {0}</p>'.format(id))
+
+    return render(request, 'inventory/item_detail.html', {
+         'item': item,
+    })
 
